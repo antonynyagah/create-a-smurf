6e776855a5cefce8ceb9a629f2feb3d9c8ae8869
Antony Nyagah <antonynyagah@Antonys-MacBook-Pro.local>
2019-12-23 12:19:31 -0600

    client mvp

---
+++ village/src/components/SmurfForm.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+class SmurfForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      age: '',
+      height: ''
+    };
+  }
+
+  addSmurf = event => {
+    event.preventDefault();
+    // add code to create the smurf using the api
+
+    this.props.edit ? this.props.update(this.state, this.props.match.params.smurfId): this.props.create(this.state);
+
+    this.setState({
+      name: '',
+      age: '',
+      height: ''
+    });
+
+    this.props.history.push("/");
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    return (
+      <div className="SmurfForm">
+        <form onSubmit={this.addSmurf}>
+          <input
+            onChange={this.handleInputChange}
+            placeholder="name"
+            value={this.state.name}
+            name="name"
+          />
+          <div className="baseline" />
+          <input
+            onChange={this.handleInputChange}
+            placeholder="age"
+            value={this.state.age}
+            name="age"
+          />
+          <div className="baseline" />
+          <input
+            onChange={this.handleInputChange}
+            placeholder="height"
+            value={this.state.height}
+            name="height"
+          />
+          <div className="baseline" />
+          <button type="submit">Add to the village</button>
+        </form>
+      </div>
+    );
+  }
+}
+export default SmurfForm;
---
+++ village/src/components/Smurfs.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import Smurf from './Smurf';
+
+class Smurfs extends Component {
+  render() {
+    return (
+      <div className="Smurfs">
+        
+        <h1>{this.props.smurfs.length} Smurf{this.props.smurfs.length > 1 ? "s" : null } are in the Village!</h1>
+          {this.props.smurfs.map(smurf => {
+            return (
+              <li>
+                <Link exact to={`/smurf/${smurf.id}`} key={smurf.id}>{smurf.name}</Link>
+              </li>
+            );
+          })}
+        
+      </div>
+    );
+  }
+}
+Smurf.defaultProps = {
+ smurfs: [],
+};
+export default Smurfs;
